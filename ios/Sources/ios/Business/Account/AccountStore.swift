536852c44cac02b7a9d7fd38459fff2778d1dfc9
Märt Samma <mrtsamma@gmail.com>
2026-05-05 18:15:44 +0300

    Add default check for account

--- ios/Sources/ios/Business/Account/AccountStore.swift
+++ ios/Sources/ios/Business/Account/AccountStore.swift
@@ -2,4 +2,5 @@ import RxSwift
 
 protocol AccountStore {
     func observeAllAccounts(for customerId: CustomerId) -> Observable<[Account]>
+    func isAccountDefault(_ accountId: AccountId) -> Single<Bool>
 }
